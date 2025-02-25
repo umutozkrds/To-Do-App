819f50fa95fae0ebc16e00b2a4cfd087b8a8f9aa
Umut Özkardeş <103787380+umutozkrds@users.noreply.github.com>
2025-02-26 00:03:14 +0300

    clear buttons are renewed

--- scirpt.js
+++ scirpt.js
@@ -2,10 +2,9 @@ const ui = new UI;
 
 
 
-
 ui.saveButton.addEventListener("click", () => {
     
-    if (ui.item.value.length > 5 || ui.subtitle.value.length > 5) {
+    if (ui.item.value.length > 5 && ui.subtitle.value.length > 5) {
 
 
         const title = ui.item.value;
@@ -19,7 +18,12 @@ ui.saveButton.addEventListener("click", () => {
                             <h5 class="card-title">${title}</h5>
                             <h6 class="card-subtitle mb-2 text-body-secondary">${subtitle}</h6>
                             <p class="card-text"></p>
-                            <button class="btn btn-danger btn-sm delete-btn">Sil</button>
+                            <button id="clear" type="button" class="btn btn-outline-danger delete-btn">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"></path>
+                                    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"></path>
+                                </svg>
+                            </button>
                         </div>
                     </div>
         
@@ -28,6 +32,7 @@ ui.saveButton.addEventListener("click", () => {
         card.querySelector(".delete-btn").addEventListener("click", function () {
             card.remove(); // HTML'den kaldır
             saveToLocalStorage(); // LocalStorage'ı güncelle
+            clearIsactive();
         });
 
         ui.todoList.appendChild(card);
@@ -41,6 +46,8 @@ ui.saveButton.addEventListener("click", () => {
     else {
         console.log("error")
     }
+
+    clearIsactive();
     
 })
 
@@ -66,6 +73,8 @@ function loadItems() {
         ui.todoList.innerHTML = "";
         localStorage.removeItem("todoList");
     }
+    clearIsactive();
+    
 }
 
 ui.clearButton.addEventListener("click", () => {
@@ -74,5 +83,13 @@ ui.clearButton.addEventListener("click", () => {
     saveToLocalStorage();
 })
 
+function clearIsactive() {
+    if (!localStorage.getItem("todoList")) {
+        ui.clearButton.style.display = "none";
+    }
+    else {
+        ui.clearButton.style.display = "block";
+    }
+}
 
 loadItems();
