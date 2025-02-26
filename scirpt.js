08444c3ab65f1f5c1b95d2924adb78b7b24defc7
Umut Özkardeş <103787380+umutozkrds@users.noreply.github.com>
2025-02-26 13:56:37 +0300

    done checkbox was added

--- scirpt.js
+++ scirpt.js
@@ -3,7 +3,7 @@ const ui = new UI;
 
 
 ui.saveButton.addEventListener("click", () => {
-    
+
     if (ui.item.value.length > 5 && ui.subtitle.value.length > 5) {
 
 
@@ -14,21 +14,45 @@ ui.saveButton.addEventListener("click", () => {
         card.className = "col-sm-6 col-md-4 col-lg-3";
         card.innerHTML = `
                     <div class="card m-1">
-                        <div class="card-body">
-                            <h5 class="card-title">${title}</h5>
-                            <h6 class="card-subtitle mb-2 text-body-secondary">${subtitle}</h6>
-                            <p class="card-text"></p>
-                            <button id="clear" type="button" class="btn btn-outline-danger delete-btn">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"></path>
-                                    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"></path>
-                                </svg>
-                            </button>
+                        <div class="card-body d-flex justify-content-between align-items-center">
+                            <div>
+                                <h5 class="card-title">${title}</h5>
+                                <h6 class="card-subtitle mb-2 text-body-secondary">${subtitle}</h6>
+                                <p class="card-text"></p>
+                                <button id="clear" type="button" class="btn btn-outline-danger delete-btn">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"></path>
+                                        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"></path>
+                                    </svg>
+                                </button>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="">
+                                <label class="form-check-label" for="flexCheckDefault">Done</label>
+                            </div>
                         </div>
                     </div>
+
         
         `;
-        
+
+        card.querySelector(".form-check-input").addEventListener("change", () => {
+            checkbox = card.querySelector(".form-check-input");
+            
+            if (checkbox.checked) {
+                card.querySelector("h5").style.textDecoration = "line-through";
+                card.querySelector("h6").style.textDecoration = "line-through";
+                card.style.opacity = 0.5;
+                
+            }
+            else {
+                card.querySelector("h5").style.textDecoration = "";
+                card.querySelector("h6").style.textDecoration = "";
+                card.style.opacity = 1;
+            }
+            
+        })
+
         card.querySelector(".delete-btn").addEventListener("click", function () {
             card.remove(); // HTML'den kaldır
             saveToLocalStorage(); // LocalStorage'ı güncelle
@@ -36,7 +60,7 @@ ui.saveButton.addEventListener("click", () => {
         });
 
         ui.todoList.appendChild(card);
-        
+
         list = ui.todoList.innerHTML;
         saveToLocalStorage();
 
@@ -48,7 +72,7 @@ ui.saveButton.addEventListener("click", () => {
     }
 
     clearIsactive();
-    
+
 })
 
 function saveToLocalStorage() {
@@ -74,7 +98,7 @@ function loadItems() {
         localStorage.removeItem("todoList");
     }
     clearIsactive();
-    
+
 }
 
 ui.clearButton.addEventListener("click", () => {
